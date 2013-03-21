82a3f40ae03735a46861ab5c3e1ca3844cdc9186
Benjamin Michael Brown <benjaminbb@gmail.com>
2013-03-20 23:47:47 -0500

    sprinkled curren_user.name around the place to liven things up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Binterest::Application.routes.draw do
   root :to => 'pages#home'
   get 'about' => 'pages#about'
 
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20130321044342_add_username_to_user.rb
@@ -0,0 +1,4 @@
+class AddUsernameToUser < ActiveRecord::Migration
+  def change
+  end
+end
