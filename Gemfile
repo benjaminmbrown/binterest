dcb444d8cf6e4c666bb53c304416bf9e15d8f327
Benjamin Michael Brown <benjaminbb@gmail.com>
2013-03-20 21:58:32 -0500

    updated w bootstrap

--- Gemfile
+++ Gemfile
@@ -12,10 +12,12 @@ group :development do
 end
 
 
+
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
 end
 
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Binterest::Application.routes.draw do
+  
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Binterest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
